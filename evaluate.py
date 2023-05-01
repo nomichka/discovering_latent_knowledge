3984d5d310c59bc45904c40504b157f70ec026f2
tcy63 <2000017814@pku.edu.cn>
2023-05-01 20:20:52 +0000

    fix evaluation for each layer and plot

--- evaluate.py
+++ evaluate.py
@@ -1,39 +1,56 @@
 from sklearn.linear_model import LogisticRegression
 from utils import get_parser, load_all_generations, CCS
+import matplotlib.pyplot as plt
+import os
 
 def main(args, generation_args):
     # load hidden states and labels
-    neg_hs, pos_hs, y = load_all_generations(generation_args)
+    # shape [num_examples, model_dim, num_layer]
+    neg_hs_all, pos_hs_all, y = load_all_generations(generation_args)
 
     # Make sure the shape is correct
-    assert neg_hs.shape == pos_hs.shape
-    neg_hs, pos_hs = neg_hs[..., -1], pos_hs[..., -1]  # take the last layer
-    if neg_hs.shape[1] == 1:  # T5 may have an extra dimension; if so, get rid of it
-        neg_hs = neg_hs.squeeze(1)
-        pos_hs = pos_hs.squeeze(1)
+    assert neg_hs_all.shape == pos_hs_all.shape
 
-    # Very simple train/test split (using the fact that the data is already shuffled)
-    neg_hs_train, neg_hs_test = neg_hs[:len(neg_hs) // 2], neg_hs[len(neg_hs) // 2:]
-    pos_hs_train, pos_hs_test = pos_hs[:len(pos_hs) // 2], pos_hs[len(pos_hs) // 2:]
-    y_train, y_test = y[:len(y) // 2], y[len(y) // 2:]
+    # Evaluate across layers
+    if args.all_layers:
+        num_layers = neg_hs_all.shape[-1]
+        ccs_accuracy_all = []
+        lr_accuracy_all = []
+        for layer in range(num_layers):
+            neg_hs = neg_hs_all[..., layer]
+            pos_hs = pos_hs_all[..., layer]
 
-    # Make sure logistic regression accuracy is reasonable; otherwise our method won't have much of a chance of working
-    # you can also concatenate, but this works fine and is more comparable to CCS inputs
-    x_train = neg_hs_train - pos_hs_train  
-    x_test = neg_hs_test - pos_hs_test
-    lr = LogisticRegression(class_weight="balanced")
-    lr.fit(x_train, y_train)
-    print("Logistic regression accuracy: {}".format(lr.score(x_test, y_test)))
+            if neg_hs.shape[1] == 1:  # T5 may have an extra dimension; if so, get rid of it
+                neg_hs = neg_hs.squeeze(1)
+                pos_hs = pos_hs.squeeze(1)
 
-    # Set up CCS. Note that you can usually just use the default args by simply doing ccs = CCS(neg_hs, pos_hs, y)
-    ccs = CCS(neg_hs_train, pos_hs_train, nepochs=args.nepochs, ntries=args.ntries, lr=args.lr, batch_size=args.ccs_batch_size, 
-                    verbose=args.verbose, device=args.ccs_device, linear=args.linear, weight_decay=args.weight_decay, 
-                    var_normalize=args.var_normalize)
+            # Very simple train/test split (using the fact that the data is already shuffled)
+            neg_hs_train, neg_hs_test = neg_hs[:len(neg_hs) // 2], neg_hs[len(neg_hs) // 2:]
+            pos_hs_train, pos_hs_test = pos_hs[:len(pos_hs) // 2], pos_hs[len(pos_hs) // 2:]
+            y_train, y_test = y[:len(y) // 2], y[len(y) // 2:]
+
+            # Make sure logistic regression accuracy is reasonable; otherwise our method won't have much of a chance of working
+            # you can also concatenate, but this works fine and is more comparable to CCS inputs
+            x_train = neg_hs_train - pos_hs_train  
+            x_test = neg_hs_test - pos_hs_test
+            lr = LogisticRegression(class_weight="balanced")
+            lr.fit(x_train, y_train)
+            lr_acc = lr.score(x_test, y_test)
+            print("Logistic regression accuracy: {}".format(lr_acc))
+
+            # Set up CCS. Note that you can usually just use the default args by simply doing ccs = CCS(neg_hs, pos_hs, y)
+            ccs = CCS(neg_hs_train, pos_hs_train, nepochs=args.nepochs, ntries=args.ntries, lr=args.lr, batch_size=args.ccs_batch_size, 
+                            verbose=args.verbose, device=args.ccs_device, linear=args.linear, weight_decay=args.weight_decay, 
+                            var_normalize=args.var_normalize)
+            
+            # train and evaluate CCS
+            ccs.repeated_train()
+            ccs_acc = ccs.get_acc(neg_hs_test, pos_hs_test, y_test)
+            print("CCS accuracy: {}".format(ccs_acc))
+            ccs_accuracy_all.append(ccs_acc)
+            lr_accuracy_all.append(lr_acc)
     
-    # train and evaluate CCS
-    ccs.repeated_train()
-    ccs_acc = ccs.get_acc(neg_hs_test, pos_hs_test, y_test)
-    print("CCS accuracy: {}".format(ccs_acc))
+    return ccs_accuracy_all, lr_accuracy_all
 
 
 if __name__ == "__main__":
@@ -50,4 +67,10 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", type=float, default=0.01)
     parser.add_argument("--var_normalize", action="store_true")
     args = parser.parse_args()
-    main(args, generation_args)
+    ccs_accs, lr_accs = main(args, generation_args)
+    plt.plot(ccs_accs)
+    plt.plot(lr_accs)
+    if not os.path.exists("figures"):
+        os.makedirs("figures")
+    plt.savefig("figures/accs")
+
